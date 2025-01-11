73b2fa634eee0a51eb7329f061445eb5c5d4664c
Ali_jan <hassanfarid667@gmail.com>
2025-01-11 11:07:34 +0500

    second commit

--- Assignment_12/app.js
+++ Assignment_12/app.js
@@ -1,21 +1,16 @@
- // Prompt the user for their date of birth
 let date = prompt("Enter your birth date (1-31):");
 let month = prompt("Enter your birth month (1-12):");
-let year = prompt("Enter your birth year (e.g., 1990):");
+let year = prompt("Enter your birth year:");
 
-// Convert the inputs to numbers
 date = parseInt(date);
-month = parseInt(month) - 1; // JavaScript months are zero-indexed
+month = parseInt(month) - 1;  
 year = parseInt(year);
 
-// Create a new Date object for the date of birth
 let birthDate = new Date(year, month, date);
 
-// Array of weekday names
 let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-// Get the day of the week
-let birthDayOfWeek = daysOfWeek[birthDate.getDay()];
+let bithDay = daysOfWeek[birthDate.getDay()];
 
-// Alert the user with the day of the week they were born
-alert("You were born on a " + birthDayOfWeek + ".");
+
+alert("You were born on a " + bithDay + ".");
